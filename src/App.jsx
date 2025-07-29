3ba2da1d2f86a83ce3018da24dc5b742e99e0c34
kishore-001 <kishoreedu07@gmail.com>
2025-07-29 22:15:11 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Image from "./pages/Image"
-import NotFound from "./pages/NotFound"
-import Navigation from "./components/Navigation"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Image from "./pages/Image";
+import NotFound from "./pages/NotFound";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/GhostX">
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -14,7 +14,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
